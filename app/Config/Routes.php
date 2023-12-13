2103d8f9b7e86c867fdc590422d6337fca665047
saidabdillah <msaidabdillah18@gamil.com>
2023-12-13 09:53:58 +0800

    Menghapus beberapa file yang tidak diperlukan lagi, karena itu latihan tugas kuliah bukan tugas uas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,14 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// $routes->get('/', 'Home::index');
-// $routes->get('/login', 'LoginController::create');
-// $routes->post('/login', 'LoginController::store');
-
-
-$routes->get('/', 'ProductController::index');
-$routes->get('/list', 'ProductController::index');
-$routes->post('add-product', 'ProductController::store');
-$routes->get('edit-product/(:num)', 'ProductController::edit/$1');
-$routes->post('update-product', 'ProductController::update');
-$routes->post('delete-product', 'ProductController::delete');
+$routes->get('/', 'Home::index');
+$routes->get('/login', 'LoginController::create');
+$routes->post('/login', 'LoginController::store');
