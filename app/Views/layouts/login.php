3a976714c02eac48c817f1352d38fa140bcb0e30
saidabdillah <msaidabdillah18@gamil.com>
2023-12-13 10:33:17 +0800

    ada yang ketinggalan

--- app/Views/layouts/login.php
+++ app/Views/layouts/login.php
@@ -15,7 +15,7 @@
 <body>
 
   <!-- Main -->
-  <!-- <main>
+  <main>
     <section class="container" id="login">
       <div class="row justify-content-center align-items-center">
         <div class="col-sm-6 col-md-4">
@@ -44,7 +44,7 @@
         </div>
       </div>
     </section>
-  </main> -->
+  </main>
   <!-- Akhir Main -->
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
